422d2a4375992bad4cac7021310e5ca1d2b6c801
Nightkun <nthe7610@gmail.com>
2024-09-17 13:51:55 +0700

    F-AniNight

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -118,14 +118,14 @@ function getImageUrl(image) {
     if (image.startsWith('http') || image.startsWith('https')) {
         return image;
     } else {
-        return `https://anime.ani-night.online/uploads/profiles/${image}`;
+        return `https://ani-night.online/uploads/profiles/${image}`;
     }
 }
 function getImageUrlBanner(image) {
     if (image.startsWith('http') || image.startsWith('https')) {
         return image;
     } else {
-        return `https://anime.ani-night.online/uploads/banners/${image}`;
+        return `https://ani-night.online/uploads/banners/${image}`;
     }
 }
 </script>
